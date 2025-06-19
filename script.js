4dee8ee46c71f4c63f9103649617bb932ea246bf
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-19 23:40:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 // Complete the js code
 function Car(make, model) {
-	this.make = make;
-	this.model = model;
-}
-Car.prototype.getMakeModel = function(){
-	return `${this.make} ${this.model}`;
+    this.make = make;
+    this.model = model;
 }
 
+// Step 2: getMakeModel Method
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
+
+// Step 3: SportsCar Constructor
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.topSpeed = topSpeed;
+    // Call the Car constructor
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
-SportsCar.prototype.getTopSpeed = function(){
-	return this.topSpeed;
-}
+
+// Step 4: getTopSpeed Method
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
+// Step 5: Set up inheritance
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
-
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
